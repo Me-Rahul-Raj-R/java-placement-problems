3dff0dcd57ac94cf7599ff635f44226596888aef
Me-Rahul-Raj-R <rahulrajan9384@gamil.com>
2025-09-18 10:23:47 +0530

    factorial of Number

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class factorial {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number for Factorial : ");
+        int num =sc.nextInt();
+        System.out.println(fact(num));//argument
+        sc.close();
+
+    }
+    static int fact(int n){//parameter
+        if(n==0 || n==1)
+        return 1;
+    return (n*fact(n-1));
+    }
+}
