51d090a09b9863604dd34ef8eca9f259062d24d7
Me-Rahul-Raj-R <rahulrajan9384@gamil.com>
2025-09-25 11:57:38 +0530

    String Basics

---
+++ Str.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Str{
+    public static void main(String[] args) {
+        char name[]={'V','S','B'};
+        String str = Arrays.toString(name);
+        System.out.println(str);
+    }
+    
+}
