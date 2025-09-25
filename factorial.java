e751ec32898a6158deb3a35d2a2aa16018333ffd
Me-Rahul-Raj-R <rahulrajan9384@gamil.com>
2025-09-25 09:25:58 +0530

    Factorial

--- factorial.java
+++ factorial.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class factorial {
+public class factorial {// recursive Case
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number for Factorial : ");
